fb6f6c9051783c5e255c273a9eac16f85bbd1fba
damiansf <damiansandhu11@gmail.com>
2019-12-07 15:08:54 -0500

    updating backend

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:8.11.3
+FROM node:8.16.2
 
 # Create app directory
 WORKDIR /usr/src/app
--- mock.js
+++ mock.js
@@ -1,25 +1,27 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var req = require('sync-request');
-var bodyParser = require('body-parser');
+let express = require("express");
+let app = express();
+let fs = require("fs");
+let bodyParser = require("body-parser");
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var server = app.listen(8080, function () {
+let server = app.listen(8080, function() {
+  let host = server.address().address;
+  let port = server.address().port;
 
-  var host = server.address().address
-  var port = server.address().port
+  console.log("Mock listening at http://%s:%s", host, port);
+});
 
-  console.log("Mock listening at http://%s:%s", host, port)
-
-})
-
-app.get('/DoStuff', function (req, res) {
-   fs.readFile( __dirname + "/jsonFiles/" + "Sample.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
-})
-
+app.get("/DoStuff", function(req, res) {
+  fs.readFile(__dirname + "/jsonFiles/" + "Sample.json", "utf8", function(
+    err,
+    data
+  ) {
+    if (err) {
+      res.send("Error");
+    } else {
+      res.send(data);
+    }
+  });
+});
